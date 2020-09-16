f0ec00d7f94bce759caf3d1a63877b62a32b4363
Britney Hanna <athina@Britneys-MacBook-Air.local>
2020-09-16 19:56:54 -0400

    Use StringBuilder to make more efficent

--- src/SpacedLogger.java
+++ src/SpacedLogger.java
@@ -2,18 +2,23 @@
 public class SpacedLogger implements Logger {
 
 	public void Log(String Log) {
+
+		StringBuilder word = new StringBuilder();
 		for (int i = 0; i < Log.length(); i++) {
-			System.out.print(Log.charAt(i) + " ");
+			word.append(Log.charAt(i)).append(" ");
 		}
+
+		System.out.println(word);
 	}
 
 	public void Error(String Error) {
-		String word = " ";
 
+		StringBuilder word2 = new StringBuilder();
 		for (int i = 0; i < Error.length(); i++) {
-			word += Error.charAt(i) + " ";
+			word2.append(Error.charAt(i)).append(" ");
 		}
-		System.out.println("ERROR: " + word);
+		System.out.println("ERROR: " + word2);
 
 	}
+
 }
