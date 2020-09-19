891b9a3788320ff911aa7b09d2aac43e5ddfc348
Britney Hanna <athina@Britneys-MacBook-Air.local>
2020-09-19 00:45:31 -0400

    Updated Asterik logger

--- src/AsteriskLogger.java
+++ src/AsteriskLogger.java
@@ -1,17 +1,16 @@
 
 public class AsteriskLogger implements Logger {
 
+	String asterik = "*******************";
+
 	public void Log(String Log) {
 		System.out.println("***" + Log + "***");
 
 	}
 
 	public void Error(String Error) {
-		System.out.println("******************");
-		System.out.println();
-		System.out.println("*** Error: " + Error + "***");
-		System.out.println();
-		System.out.println("******************");
+
+		System.out.println(asterik + '\n' + '\n' + "*** Error: " + Error + "***" + '\n' + '\n' + asterik);
 
 	}
 
